883cb51d887de871257b03309941ab64a21e9cf1
jdvorak23 <dvorakj3@post.cz>
2025-04-18 15:51:52 +0200

    InputTemplate

---
+++ src/Entities/General/InputTemplate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Cesys\CakeEntities\Entities\General;
+
+use Cesys\CakeEntities\Entities\CakeEntity;
+use Nette\Utils\DateTime;
+
+class InputTemplate extends CakeEntity
+{
+    public int $id;
+
+    public string $company;
+
+    public string $type;
+
+    public string $invoiceType;
+    public ?DateTime $created;
+    public ?DateTime $modified;
+}
---
+++ src/Entities/General/InputTemplateProperty.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Cesys\CakeEntities\Entities\General;
+
+use Cesys\CakeEntities\Entities\CakeEntity;
+use Nette\Utils\DateTime;
+
+class InputTemplateProperty extends CakeEntity
+{
+    public int $id;
+
+    public int $inputTemplateId;
+    public string $name;
+
+    public string $pattern;
+    public ?DateTime $created;
+    public ?DateTime $modified;
+}
