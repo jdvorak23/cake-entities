fc2541284f641bbe83e076be5323074f9209ab84
jdvorak23 <dvorakj3@post.cz>
2025-06-05 18:18:00 +0200

    MoneyTransaction

--- src/Entities/EFolder/MoneyTransaction.php
+++ src/Entities/EFolder/MoneyTransaction.php
@@ -10,10 +10,23 @@ use Nette\Utils\DateTime;
 
 class MoneyTransaction extends CakeEntity
 {
-	const TypeTransfer = 'transfer';
-	const TypeCard = 'card';
-	const TypeCash = 'cash';
-	const TypeVoucher = 'voucher';
+	public const TypeTransfer = 'transfer';
+	public const TypeCard = 'card';
+	public const TypeCash = 'cash';
+	public const TypeVoucher = 'voucher';
+
+	public const Types = [
+		self::TypeCash => self::TypeCash,
+		self::TypeTransfer => self::TypeTransfer,
+		self::TypeCard => self::TypeCard,
+		self::TypeVoucher => self::TypeVoucher,
+	];
+
+	public const ProspectiveTypeDeposit = 'deposit';
+	public const ProspectiveTypeSupplement = 'supplement';
+
+	public const ProspectiveTypeCommission = 'commission';
+
 	
 	public int $id;
 	
@@ -45,6 +58,8 @@ class MoneyTransaction extends CakeEntity
 	 * @var bool
 	 */
 	public bool $isProspective;
+
+	public ?string $prospectiveType;
 
 	public bool $checked;
 
@@ -92,5 +107,57 @@ class MoneyTransaction extends CakeEntity
 			return $exchangeRate->convertFrom($this->amount);
 		}
 		return $this->amount;
+	}
+
+	public function isEditable(): bool
+	{
+		if ($this->checked) {
+			// Transakce, která je checked nemůže být měněna
+			return false;
+		}
+		if ($this->prospectiveType) {
+			// Výhledová transakce nemá jít editovat
+			return false;
+		}
+		if ($this->fBankTransactionId) {
+			// Transakce vytvořená z FBankTransaction -> nelze editovat
+			return false;
+		}
+		return true;
+	}
+
+	public function isCheckable(): bool
+	{
+		if ($this->checked) {
+			// Transakce, která je checked nemůže být měněna
+			return false;
+		}
+		if ($this->amount === null) {
+			// nezadaná částka, nemůže být checked
+			return false;
+		}
+		if (in_array($this->prospectiveType, [self::ProspectiveTypeDeposit, self::ProspectiveTypeSupplement])) {
+			// Výhledová transakce deposit / supplement, nemá být checkable
+			return false;
+		}
+		$today = new DateTime('today');
+		if ($this->date > $today) {
+			// Budoucí transakce nemůže být checked
+			return false;
+		}
+		return true;
+	}
+
+	public function isDeletable(): bool
+	{
+		if ($this->checked) {
+			// Transakce, která je checked nemůže být měněna
+			return false;
+		}
+		if ($this->prospectiveType) {
+			// Výhledová transakce s prospectiveType nelze smazat
+			return false;
+		}
+		return true;
 	}
 }
